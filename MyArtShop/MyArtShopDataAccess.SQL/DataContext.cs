e8fda2c4a52bdfe3bb4f66ffbbd0141f508a1b9f
Shere Crossman <63079719+Sherekin2012@users.noreply.github.com>
2020-07-29 20:44:07 -0500

    Enabled SQL-DB and initial migration

---
+++ MyArtShop/MyArtShopDataAccess.SQL/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using MyArtShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyArtShopDataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Product>  Products { get; set; }
+        public DbSet<ProductCategory>  ProductCategories { get; set; }
+    }
+}
+
---
+++ MyArtShop/MyArtShopDataAccess.SQL/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace MyArtShopDataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MyArtShopDataAccess.SQL.DataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MyArtShopDataAccess.SQL.DataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
